09085a0f90d302948fb87b95f0396b1582a98e1c
Alexs18 <mclexis18@gmail.com>
2026-05-28 22:17:06 -0500

    porfolio

---
+++ src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+import {Main} from './components/main/main';
+
+export const routes: Routes = [
+    {
+        path:'main',
+        component:Main
+    },
+    { path: '**', redirectTo: 'main' } 
+];
